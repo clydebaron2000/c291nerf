339e810218eb110fe868b20d5c2814f71836d5be
clydebaronrapinan <clydebaron2000@gmail.com>
2022-03-15 15:12:11 -0700

    updated ez run

--- easy_run.sh
+++ easy_run.sh
@@ -1,7 +1,7 @@
 echo "setting up environment"
 bash set_up.sh
+echo "clearing logs"
+rm -rf logs/*
 echo "running experiments"
 echo "bottles:"
 python main.py --config ./config/bottles.txt
-echo "lego:"
-python main.py --config ./config/lego.txt
