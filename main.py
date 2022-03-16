45456a91612d6ffa3bc697320ad90eb94bb19712
clydebaronrapinan <clydebaron2000@gmail.com>
2022-03-15 20:15:32 -0700

    index

--- main.py
+++ main.py
@@ -738,9 +738,9 @@ def train():
             os.makedirs(testsavedir, exist_ok=True)
             # print('test poses shape', poses[i_test].shape)
             # TODO: CHANGE
-            print('test poses shape', poses[30].shape)
+            print('test poses shape', poses[[30]].shape)
             with torch.no_grad():
-                render_path(torch.Tensor(poses[30]).to(device), hwf, K, args.chunk, render_kwargs_test,
+                render_path(torch.Tensor(poses[[30]]).to(device), hwf, K, args.chunk, render_kwargs_test,
                                 # gt_imgs=images[i_test], 
                                 savedir=testsavedir)
             print('Saved test set')
