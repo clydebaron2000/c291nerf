d578e314dfa369eb55e9944d844f91b899678c97
clydebaron2000 <clydebaron2000@gmail.com>
2022-03-19 20:15:50 +0000

    a

---
+++ GPU0_jobs.sh
@@ -0,0 +1,2 @@
+python main.py --config ./configs/bottles_official.txt --gpu=0 --n_iters=200000 --expname=bottles_official_100k
+ps
---
+++ GPU2_jobs.sh
@@ -0,0 +1,1 @@
+python main.py --config ./configs/bottles_official.txt --gpu=2 --n_iters=100000 --multires=15 --multires_views=6 --expname=bottles_official_multiperm1.5x
